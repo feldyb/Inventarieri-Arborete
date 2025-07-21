2b3e1e633bf5486f2cd5899a2753aa9b78e6c520
feldyb <68964118+feldyb@users.noreply.github.com>
2025-07-21 14:28:26 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("arbori-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js",
+        "manifest.json",
+        "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
